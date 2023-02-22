9ee8b75263f7b1495c40949117f055ab9a549312
tsubasa <m@tsubasa.me>
2023-02-23 01:32:35 +0900

    changed rootDir

--- ts-jsx-vite-eslint-prettier/vite.config.ts
+++ ts-jsx-vite-eslint-prettier/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+  },
   resolve: {
     alias: [
       {
