3d4b40ded9061cc3be89c47897f47532a08be4c7
tsubasa <m@tsubasa.me>
2023-02-23 01:22:00 +0900

    changed rootDir

--- ts-sass-static-vite-eslint-stylelint-prettier/vite.config.ts
+++ ts-sass-static-vite-eslint-stylelint-prettier/vite.config.ts
@@ -2,6 +2,11 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+  },
   resolve: {
     alias: [
       {
