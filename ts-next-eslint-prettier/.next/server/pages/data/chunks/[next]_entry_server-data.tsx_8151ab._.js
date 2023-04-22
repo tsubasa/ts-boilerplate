e5490d5488abb27b4ebde0aeb8c17abcaff17dec
tsubasa <m@tsubasa.me>
2023-04-22 11:43:05 +0900

    removed .next

--- ts-next-eslint-prettier/.next/server/pages/data/chunks/[next]_entry_server-data.tsx_8151ab._.js
+++
@@ -1,9 +0,0 @@
-Error.stackTraceLimit = 100;
-global.self = global;
-require("./.env_b53fce.js");
-require("./[turbopack-node]_ipc_index.ts_47c3bc._.js");
-require("./[turbopack-node]_compiled_stacktrace-parser_index_6f7819.js");
-require("./[next]_internal_page-server-handler.tsx_207b2e._.js");
-require("./[next]_entry_server-data.tsx_a1456a._.js");
-require("./[next]_entry_server-data.tsx_f226e0._.js");
-require("./[next]_entry_server-data.tsx_5e18f3._.js");
--- ts-next-eslint-prettier/.next/server/pages/data/chunks/src_pages_index.tsx_3fd01b._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["chunks/src_pages_index.tsx_3fd01b._.js", {
-
-"[project]/src/pages/index.tsx (ecmascript)": (function({ r: __turbopack_require__, x: __turbopack_external_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_cjs__, g: global, __dirname, m: module, e: exports }) { !function() {
-
-
-}.call(this) }),
-}]);
-
-//# sourceMappingURL=src_pages_index.tsx_3fd01b._.js.map
